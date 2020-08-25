bb3ca7de43a92f855849a2cba9d0c4c0b0edd749
geraldprambudi <geraldprambudi94@gmail.com>
2020-08-25 23:32:49 +0700

    install flask, jinja, layout, fetch data no database

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'belajar flask',
+            'body': 'yuk belajar flask',
+            'author': 'gerald prambudi',
+            'create_date': '15-08-2020 '
+        },
+        {
+            'id':2,
+            'title': 'belajar laravel',
+            'body': 'yuk belajar laravel',
+            'author': 'gerald prambudi',
+            'create_date': '16-08-2020 '
+        },
+        {
+            'id':3,
+            'title': 'belajar codeigniter',
+            'body': 'yuk belajar codeigniter',
+            'author': 'gerald prambudi',
+            'create_date': '17-08-2020 '
+        }
+    ]
+
+    return articles
